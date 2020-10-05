2290d3b53fd779cba1a314c354558a9c8b3fde6d
zakaazimli <50147514+zakaazimli@users.noreply.github.com>
2020-10-05 16:28:37 +0400

    Create IMunicipalityTaxesService.cs

---
+++ IMunicipalityTaxesService.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace MunicipalityTaxes
+{
+    [ServiceContract]
+    public interface IMunicipalityTaxesService
+    {
+        [OperationContract]
+        TaxScheduleActionResult<TaxScheduleInsertionResult> InsertTaxScheduleDetails (MunicipalityTaxDetails tax);
+
+        /// <summary>
+        /// Get the specific municipality tax that applies on the given date
+        /// </summary>
+        /// <param name="Muncipality">The name of the muncipality to check the tax of</param>
+        /// <param name="at">The date on which to get the taxes for</param>
+        /// <returns>The tax that applies/applied for the given <paramref name="Muncipality"/> on the given date, or <code>null</code> if no schedules exist that match the given input parameters</returns>
+        [OperationContract]
+        double? GetTax (string Muncipality, DateTime at);
+
+        [OperationContract]
+        TaxScheduleActionResult<TaxScheduleUpdateResult> UpdateTaxScheduleDetails (MunicipalityTaxDetails tax);
+    }
+
+}

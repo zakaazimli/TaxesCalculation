7a17952d9a07e11d54d7fb0b6d7b73ecfd78af9b
zakaazimli <50147514+zakaazimli@users.noreply.github.com>
2020-10-05 16:17:27 +0400

    Create TaxScheduleActionResult.cs

---
+++ TaxScheduleActionResult.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MunicipalityTaxes
+{
+    [DataContract]
+    public class TaxScheduleActionResult<T>
+    {
+        [DataMember]
+        public readonly TaxScheduleValidationResult Validity;
+        [DataMember]
+        public readonly T ActionResult;
+
+        public TaxScheduleActionResult(TaxScheduleValidationResult validityStatus, T actionResult)
+        {
+            Validity = validityStatus;
+            ActionResult = actionResult;
+        }
+
+        public new string ToString()
+        {
+            return $"Validity: {Validity}, {typeof(T).Name}: {ActionResult}";
+        }
+    }
+
+    [DataContract]
+    public enum TaxScheduleInsertionResult
+    {
+        [EnumMember]
+        InsertionNotAttempted,
+        [EnumMember]
+        Success,
+        [EnumMember]
+        TaxScheduleAlreadyExists,
+        [EnumMember]
+        UnknownFailure,
+    }
+
+    [DataContract]
+    public enum TaxScheduleUpdateResult
+    {
+        [EnumMember]
+        UpdateNotAttempted,
+        [EnumMember]
+        Success,
+        [EnumMember]
+        ExistingTaxScheduleNotFound,
+        [EnumMember]
+        UnknownFailure,
+    }
+}

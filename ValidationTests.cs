602ee8ef0c7a18d2f62714054d13893c5284e745
zakaazimli <50147514+zakaazimli@users.noreply.github.com>
2020-10-05 16:46:13 +0400

    Create ValidationTests.cs

---
+++ ValidationTests.cs
@@ -0,0 +1,115 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MunicipalityTaxes;
+
+namespace TaxingTests
+{
+    [TestClass]
+    public class ValidationTests
+    {
+        [TestMethod]
+        public void NoMunicipality ()
+        {
+            var validator = new TaxScheduleValidator();
+
+            var tax = new MunicipalityTaxSchedule(municipality: "", begin: new DateTime(2016, 06, 03), frequency: ScheduleFrequency.Daily);
+            var status = validator.ValidateTaxSchedule(tax);
+            Assert.AreEqual(TaxScheduleValidationResult.MunicipalityInvalid, status);
+
+            tax = new MunicipalityTaxSchedule(municipality: "   ", begin: new DateTime(2016, 06, 03), frequency: ScheduleFrequency.Daily);
+            status = validator.ValidateTaxSchedule(tax);
+            Assert.AreEqual(TaxScheduleValidationResult.MunicipalityInvalid, status);
+
+            tax = new MunicipalityTaxSchedule(municipality: null, begin: new DateTime(2016, 06, 03), frequency: ScheduleFrequency.Daily);
+            status = validator.ValidateTaxSchedule(tax);
+            Assert.AreEqual(TaxScheduleValidationResult.MunicipalityInvalid, status);
+        }
+        
+        [TestMethod]
+        public void ScheduleDateCheck ()
+        {
+            var validator = new TaxScheduleValidator();
+
+            #region Daily
+            var tax = new MunicipalityTaxSchedule(municipality: "Test", begin: new DateTime(2016, 06, 03), frequency: ScheduleFrequency.Daily);
+            var status = validator.ValidateTaxSchedule(tax);
+            Assert.AreEqual(TaxScheduleValidationResult.Valid, status);
+            tax = new MunicipalityTaxSchedule(municipality: "Test", begin: new DateTime(2016, 06, 01), frequency: ScheduleFrequency.Daily);
+            status = validator.ValidateTaxSchedule(tax);
+            Assert.AreEqual(TaxScheduleValidationResult.Valid, status);
+            #endregion
+
+            #region Weekly
+            tax = new MunicipalityTaxSchedule(municipality: "Test", begin: new DateTime(2016, 06, 05), frequency: ScheduleFrequency.Weekly);
+            status = validator.ValidateTaxSchedule(tax);
+            Assert.AreEqual(TaxScheduleValidationResult.Valid, status);
+            tax = new MunicipalityTaxSchedule(municipality: "Test", begin: new DateTime(2016, 06, 06), frequency: ScheduleFrequency.Weekly);
+            status = validator.ValidateTaxSchedule(tax);
+            Assert.AreEqual(TaxScheduleValidationResult.DateUnsuitableForSchedule, status);
+            tax = new MunicipalityTaxSchedule(municipality: "Test", begin: new DateTime(2016, 06, 04), frequency: ScheduleFrequency.Weekly);
+            status = validator.ValidateTaxSchedule(tax);
+            Assert.AreEqual(TaxScheduleValidationResult.DateUnsuitableForSchedule, status);
+            #endregion
+
+            #region Monthly
+            tax = new MunicipalityTaxSchedule(municipality: "Test", begin: new DateTime(2016, 06, 01), frequency: ScheduleFrequency.Monthly);
+            status = validator.ValidateTaxSchedule(tax);
+            Assert.AreEqual(TaxScheduleValidationResult.Valid, status);
+            tax = new MunicipalityTaxSchedule(municipality: "Test", begin: new DateTime(2016, 06, 06), frequency: ScheduleFrequency.Monthly);
+            status = validator.ValidateTaxSchedule(tax);
+            Assert.AreEqual(TaxScheduleValidationResult.DateUnsuitableForSchedule, status);
+            tax = new MunicipalityTaxSchedule(municipality: "Test", begin: new DateTime(2016, 06, 05), frequency: ScheduleFrequency.Monthly);
+            status = validator.ValidateTaxSchedule(tax);
+            Assert.AreEqual(TaxScheduleValidationResult.DateUnsuitableForSchedule, status);
+            #endregion
+
+            #region Yearly
+            tax = new MunicipalityTaxSchedule(municipality: "Test", begin: new DateTime(2016, 01, 01), frequency: ScheduleFrequency.Yearly);
+            status = validator.ValidateTaxSchedule(tax);
+            Assert.AreEqual(TaxScheduleValidationResult.Valid, status);
+            tax = new MunicipalityTaxSchedule(municipality: "Test", begin: new DateTime(2016, 02, 01), frequency: ScheduleFrequency.Yearly);
+            status = validator.ValidateTaxSchedule(tax);
+            Assert.AreEqual(TaxScheduleValidationResult.DateUnsuitableForSchedule, status);
+            tax = new MunicipalityTaxSchedule(municipality: "Test", begin: new DateTime(2016, 01, 02), frequency: ScheduleFrequency.Yearly);
+            status = validator.ValidateTaxSchedule(tax);
+            Assert.AreEqual(TaxScheduleValidationResult.DateUnsuitableForSchedule, status);
+            tax = new MunicipalityTaxSchedule(municipality: "Test", begin: new DateTime(2016, 12, 31), frequency: ScheduleFrequency.Yearly);
+            status = validator.ValidateTaxSchedule(tax);
+            Assert.AreEqual(TaxScheduleValidationResult.DateUnsuitableForSchedule, status);
+            #endregion
+
+            #region Time
+            tax = new MunicipalityTaxSchedule(municipality: "Test", begin: new DateTime(2016, 03, 04, 5, 6, 7), frequency: ScheduleFrequency.Daily);
+            status = validator.ValidateTaxSchedule(tax);
+            Assert.AreEqual(TaxScheduleValidationResult.DateUnsuitableForSchedule, status);
+            tax = new MunicipalityTaxSchedule(municipality: "Test", begin: new DateTime(2016, 03, 04, 12, 0, 0), frequency: ScheduleFrequency.Daily);
+            status = validator.ValidateTaxSchedule(tax);
+            Assert.AreEqual(TaxScheduleValidationResult.DateUnsuitableForSchedule, status);
+            #endregion
+        }
+
+        [TestMethod]
+        public void TaxAmountChecks ()
+        {
+            var validator = new TaxScheduleValidator();
+
+            var schedule = new MunicipalityTaxSchedule(municipality: "Test", begin: new DateTime(2016, 06, 03), frequency: ScheduleFrequency.Daily);
+            var tax = new MunicipalityTaxDetails() { MunicipalitySchedule = schedule, TaxAmount = 0.1 };
+            var status = validator.ValidateTaxDetails(tax);
+            Assert.AreEqual(TaxScheduleValidationResult.Valid, status);
+
+            tax.TaxAmount = 0;
+            status = validator.ValidateTaxDetails(tax);
+            Assert.AreEqual(TaxScheduleValidationResult.Valid, status);
+
+            tax.TaxAmount = -0.1;
+            status = validator.ValidateTaxDetails(tax);
+            Assert.AreEqual(TaxScheduleValidationResult.TaxAmountInvalid, status);
+
+            schedule = new MunicipalityTaxSchedule(municipality: null, begin: new DateTime(2016, 06, 03), frequency: ScheduleFrequency.Daily);
+            tax = new MunicipalityTaxDetails() { MunicipalitySchedule = schedule, TaxAmount = 0.1 };
+            status = validator.ValidateTaxDetails(tax);
+            Assert.AreEqual(TaxScheduleValidationResult.MunicipalityInvalid, status);
+        }
+    }
+}

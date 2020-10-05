3ad57f53c031f8e8065f2ace492c72e131a48864
zakaazimli <50147514+zakaazimli@users.noreply.github.com>
2020-10-05 16:30:31 +0400

    Create ITaxScheduleValidator.cs

---
+++ ITaxScheduleValidator.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MunicipalityTaxes
+{
+    interface ITaxScheduleValidator
+    {
+        TaxScheduleValidationResult ValidateTaxSchedule (MunicipalityTaxSchedule taxSchedule);
+        TaxScheduleValidationResult ValidateTaxDetails (MunicipalityTaxDetails tax);
+    }
+}
